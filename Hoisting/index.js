fb54ee902582668b80ecedd9fb9bdaee0863b578
Manik <mkmistryhd2019@gmail.com>
2023-12-07 15:48:03 +0530

    hoisting

---
+++ Hoisting/index.js
@@ -0,0 +1,16 @@
+console.log(a);
+console.log(b);
+console.log(c);
+abc();
+
+var a=10;
+let b=20;
+const c=30;
+
+function abc(a,b,c,d=10){
+    console.log(a+b+c+d);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
