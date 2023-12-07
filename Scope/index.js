14e2b5683ee4cdf25104bcf27ff72b1d31d018a0
Manik <mkmistryhd2019@gmail.com>
2023-12-07 16:17:29 +0530

    scope

--- Scope/index.js
+++ Scope/index.js
@@ -1,20 +1,41 @@
-// Global scope
-// Block scope
+// // Global scope
+// var a =10;//it can be access through out the whole code. cuz it's a global scoped variable 
 
-var a =10;//it can be access through out the whole code. cuz it's a global scoped variable 
+// // Function scope
+// function fnScope(){
+//     var b=10;
+//     console.log(a);
+//     console.log(b);
+// }
+// fnScope();
+// console.log(b);
 
-// Function scope
+// // Block scope
+// {
+//     let x=30;
+//     let y=20;
+// }
+// console.log(x+y);
 
-function fnScope(){
-    var b=10;
-    console.log(a);
-    console.log(b);
+// Lexical scope
+let name="manik"
+function print(){
+    console.log(name);
 }
-fnScope();
-console.log(b);
+print()
 
-{
-    let x=30;
-    let y=20;
-}
-console.log(x+y);
+
+// Local Scope
+function exampleFunction() {
+    // This variable has local scope to the exampleFunction
+    let localVar = "I am a local variable";
+    if (true) {
+      // This variable also has local scope to the if block
+      let blockVar = "I am a block-scoped variable";
+      console.log(blockVar);
+    }
+    // Accessing localVar is fine within the function
+    console.log(localVar);
+  }
+  exampleFunction();
+  
