2eb5fca59c57b5aa611308007331be753ff65ab3
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-09 20:02:12 +0530

    Function Ecxpression

---
+++ Function/FunctionExpression.js
@@ -0,0 +1,11 @@
+//Regular Function
+const fn=function hello(){
+    console.log("Function Expression");
+}
+fn();
+
+// Arrow Function
+const fn2= hello=()=>{
+    console.log("Function Expression2");
+}
+fn2();
