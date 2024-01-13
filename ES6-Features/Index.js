50e6cc5d004fa36ab999db9b569b3203f0b34adc
Manik Mistry <mkmistryhd2019@gmail.com>
2024-01-13 20:45:01 +0530

    ES6 Features

---
+++ ES6-Features/Index.js
@@ -0,0 +1,28 @@
+// Arrow functions
+const ArrowFunction=()=>{
+    console.log("I am Arrow function")
+}
+ArrowFunction();
+// Destructuring
+// Template literals
+// Default parameters
+// Classes
+// Promises
+// Let and const
+// Modules
+// Rest parameters
+// Object literals
+// Arrays
+// Spread operator
+// String
+// Multiple line string
+// Symbols
+// Block-scoped variables
+// For...of loop
+// Generators
+// Iterators
+// Objects
+// Rest operators
+// Const and let keywords
+// Let
+// Maps
